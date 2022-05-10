b849d6a6136c15bf9fb0ceb542e7993e4b76734d
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-05-10 20:14:35 -0300

    Aula Linguagem de programação UNOPAR javascript

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function teste(){
     console.log('oi')
 }
+
+function validaForm (){
+    if(document.getElementById("nome").value.length < 3){
+    alert('Por favor, preencha o campo nome');
+    document.getElementById("nome").focus();
+    return false
+    }
+    }
