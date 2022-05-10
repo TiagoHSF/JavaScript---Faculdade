b2d9b1247c78f9ec1951ffe8100f4c95695c2da8
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-05-10 20:43:13 -0300

    Javascript PRONTO

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-function teste(){
-    console.log('oi')
-}
+var nomeUser = "Teste";
+var count = 0;
 
 function validaForm (){
     if(document.getElementById("nome").value.length < 3){
-    alert('Por favor, preencha o campo nome');
-    document.getElementById("nome").focus();
-    return false
+        alert('Por favor, preencha o campo nome');
+        document.getElementById("nome").focus();
     }
+    if(document.getElementById("password").value.length === 0){
+        alert('Por favor, preencha o campo senha');
+        document.getElementById("password").focus();
     }
+    if(document.getElementById("password").value == "123" && document.getElementById("nome").value == 'teste@teste.com'){
+        contador();
+        alert('Usuário Logado!');
+        alert('Olá!' + nomeUser);
+        alert('Logado pela ' + count + ' vez nesta seção');
+    } else {
+        alert('Usuário ou senha incorretos!');
+    }
+    return false;
+}
+
+function contador (){
+    count++;
+}
